500e151f469f6ff277e6696eb5cc2e666f7cac7d
askarpramod <aaskar.pramod113@gmail.com>
2024-10-24 15:12:08 +0530

    initial code

---
+++ LSP.Tests/TestGenericUserDBAccess.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace LSP.Tests
+{
+    internal class TestGenericUserDBAccess
+    {
+        private const string WriteToDatabase = "write to database";
+        private const string ChangeInDatabase = "change in database";
+
+        [Test]
+        public void ShouldNotGetValueOfAccessRightWhenRightIsProtectedTest()
+        {
+            HashSet<string> protectedRights = new HashSet<string>();
+            protectedRights.Add(WriteToDatabase);
+
+            GenericUser genericUser = new GenericUser(protectedRights);
+            genericUser.SetupAccessRight(WriteToDatabase, true);
+
+            Assert.False(genericUser.GetValueOfAccessRight(WriteToDatabase));
+        }
+
+        [Test]
+        public void ShouldGetValueOfAccessRightWhenRightIsNotProtectedTest()
+        {
+            GenericUser genericUser = new GenericUser(new HashSet<string>());
+            genericUser.SetupAccessRight(ChangeInDatabase, true);
+
+            Assert.True(genericUser.GetValueOfAccessRight(ChangeInDatabase));
+        }
+    }
+}
---
+++ LSP.Tests/TestPowerUserDBAccess.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LSP.Tests
+{
+    internal class TestPowerUserDBAccess
+    {
+        [Test]
+        public void PowerUserTest()
+        {
+            var powerUser = new PowerUser();
+
+            DatabaseGateway.WriteToDBForce(powerUser, "payload");
+
+            Assert.That("payload", Is.EqualTo(DatabaseGateway.ReadFromDB()));
+        }
+    }
+}
